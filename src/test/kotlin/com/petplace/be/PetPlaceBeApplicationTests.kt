78e578d595a1012a52060d196091db4e591979ba
pamyferret <pamyferret@gmail.com>
2022-10-16 18:35:19 +0900

    init

---
+++ src/test/kotlin/com/petplace/be/PetPlaceBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.petplace.be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetPlaceBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
