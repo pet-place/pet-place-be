2892197cbae8dfb82332f5e6a9a4c4567f8c58b2
ssyoni <jsy950601@gmail.com>
2022-12-21 00:22:18 +0900

    refactor: place 등록/수정 코드 수정
    - modify
    PlaceController.kt

--- src/main/kotlin/com/petplace/be/place/controller/PlaceController.kt
+++ src/main/kotlin/com/petplace/be/place/controller/PlaceController.kt
@@ -1,14 +1,15 @@
 package com.petplace.be.place.controller
 
 import com.petplace.be.common.response.BaseResponse
-import com.petplace.be.place.dto.result.PlaceResult
 import com.petplace.be.place.dto.param.PlaceSaveParam
 import com.petplace.be.place.dto.param.PlaceUpdateParam
+import com.petplace.be.place.dto.result.PlaceResult
 import com.petplace.be.place.dto.result.PlaceSaveResult
 import com.petplace.be.place.dto.result.PlaceUpdateResult
 import com.petplace.be.place.service.PlaceService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Place", description = "place API")
@@ -18,8 +19,9 @@ class PlaceController(
     private val placeService: PlaceService
 ) {
     @Operation(summary = "플레이스 등록", description = "플레이스가 등록됩니다.")
-    @PostMapping
-    fun createPlace(param: PlaceSaveParam): BaseResponse<PlaceSaveResult> {
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun createPlace(
+        param: PlaceSaveParam): BaseResponse<PlaceSaveResult> {
         val response = placeService.savePlace(param)
         return BaseResponse(response);
     }
@@ -32,7 +34,7 @@ class PlaceController(
     }
 
     @Operation(summary = "플레이스 수정", description = "플레이스가 수정됩니다.")
-    @PutMapping()
+    @PutMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun updatePlace(param: PlaceUpdateParam): BaseResponse<PlaceUpdateResult> {
         val response = placeService.updatePlace(param);
         return BaseResponse(response);
