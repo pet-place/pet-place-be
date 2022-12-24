3c60d75fbec96b302c660318718b71b377e6bd20
ssyoni <jsy950601@gmail.com>
2022-12-24 22:44:37 +0900

    feat: Pet 등록/수정/삭제/조회
    - add
    PetResult.kt
    PetSaveParam.kt
    PetUpdateParam.kt
    
    - modify
    Pet.kt
    PetController.kt
    PetService.kt
    ErrorCode.kt

--- src/main/kotlin/com/petplace/be/pet/service/PetService.kt
+++ src/main/kotlin/com/petplace/be/pet/service/PetService.kt
@@ -3,34 +3,74 @@ package com.petplace.be.pet.service
 import com.petplace.be.common.enums.ErrorCode
 import com.petplace.be.common.exception.CommonException
 import com.petplace.be.entity.Pet
-import com.petplace.be.pet.PetSaveParam
+import com.petplace.be.pet.dto.param.PetSaveParam
+import com.petplace.be.pet.dto.param.PetUpdateParam
+import com.petplace.be.pet.dto.result.PetResult
 import com.petplace.be.pet.repository.PetRepository
 import com.petplace.be.place.repository.PlaceRepository
+import com.petplace.be.utils.FileUploader
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 class PetService(
     private val petRepository: PetRepository,
-    private val placeRepository: PlaceRepository
+    private val placeRepository: PlaceRepository,
+    private val fileUploader: FileUploader
 ) {
-
-    fun savePet(param: PetSaveParam){
-        //place 조회
+    @Transactional
+    fun savePet(param: PetSaveParam): PetResult {
         val savedPlace = placeRepository.findById(param.placeId)
             .orElseThrow {throw CommonException(ErrorCode.PLACE_NOT_FOUND) }
 
-        //pet 저장
-        val pet = Pet(
+        val pet = petRepository.save(
+            Pet(
             name = param.name,
             age = param.age,
             gender = param.gender,
             characteristic = param.characteristic,
             liked = param.liked,
             disliked = param.disliked,
-            place = savedPlace
+            place = savedPlace,
+            profileImage = null
+            )
         )
 
-        petRepository.save(pet)
+        var uploadedUrl: String = ""
+        if (param.profileImage != null){
+            uploadedUrl = uploadProfileImage(param.profileImage!!, pet.place.id!!, pet.id!!)
+        }
 
+        pet.updateProfileImage(uploadedUrl);
+
+        return PetResult.generateFrom(pet)
+    }
+
+    @Transactional
+    fun updatePet(param: PetUpdateParam): PetResult{
+        val pet = petRepository.findById(param.id).orElseThrow { throw CommonException(ErrorCode.PET_NOT_FOUND) }
+
+        pet.updateProfileImage(uploadProfileImage(param.profileImage!!, param.id, param.placeId))
+        pet.update(param)
+
+        return PetResult.generateFrom(pet)
+    }
+
+    fun findByPetId(id: Long): PetResult{
+        return PetResult.generateFrom(
+            petRepository.findById(id).orElseThrow { throw CommonException(ErrorCode.PET_NOT_FOUND) }
+        )
+    }
+
+    @Transactional
+    fun deleteById(id: Long){
+        petRepository.deleteById(id)
+    }
+
+    private fun uploadProfileImage(profileImage: MultipartFile, placeId: Long, petId: Long): String{
+        var fileName = "pet-profile-$petId"
+        val key = "$placeId/$fileName"
+        return fileUploader.upload(profileImage, key)
     }
 }
