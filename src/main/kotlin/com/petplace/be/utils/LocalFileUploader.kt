d084485a05016646acf14c8c37fa5a9128b355ff
ssyoni <jsy950601@gmail.com>
2023-01-04 23:15:14 +0900

    fix: S3 파일 저장 시 확장자 함께 저장되게 수정
    - modify
    LocalFileUploader.kt
    S3Client.kt

--- src/main/kotlin/com/petplace/be/utils/LocalFileUploader.kt
+++ src/main/kotlin/com/petplace/be/utils/LocalFileUploader.kt
@@ -14,7 +14,8 @@ class LocalFileUploader: FileUploader {
 
     // TODO: 파일 이름에 아이디를 넣는 것에 대한 협의 필요(폴더 생성 어려움) -> S3 업로드 로직에서 폴더 생성 문제 없다면 문제 없음
     override fun upload(sourceFile: MultipartFile, destinationFileName: String): String {
-        val ext = sourceFile.originalFilename!!.split(".")[1]
+        val filename = sourceFile.originalFilename!!
+        val ext = filename.substring(filename.lastIndexOf(".") + 1)
         val destinationFile = File("${destinationFileUriPrefix}${File.separator}${destinationFileName}.${ext}")
         destinationFile.mkdirs()
         sourceFile.transferTo(destinationFile)
